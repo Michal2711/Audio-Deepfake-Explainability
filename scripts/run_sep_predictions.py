6fe58e6cec10f204b39475d64249f33b033d5436
Michał Kopeć <01159326@pw.edu.pl>
2026-03-28 12:20:45 +0100

    script to predict separated audio

--- scripts/run_sep_predictions.py
+++ scripts/run_sep_predictions.py
@@ -31,50 +31,59 @@ from sonics_api import LocalSonnics, RemoteSonnics
 from gradio_client import Client, handle_file
 
 def main():
-    orig_data = "Data/FakeRealMusicOriginal/ElevenLabs/2._Travis_Scott_Sico_Mode_-_Run_This_Town_Wariant_1_2025-08-22T205303.mp3"
-    dataset_path = "results/AudioLIME/FakeRealMusicOriginal/AudioLIME_500_samples_full_track/full_track/ElevenLabs/2__Travis_Scott_Sico_Mode_-_Run_This_Town_Wariant_1_2025-08-22T205303/reversed_separated_components"
-
+    dataset_path = "results/AudioLIME/FakeRealMusicOriginal/AudioLIME_500_samples_full_track/full_track/" # ElevenLabs/2__Travis_Scott_Sico_Mode_-_Run_This_Town_Wariant_1_2025-08-22T205303/reversed_separated_components"
+    output_json_path = "results/AudioLIME/FakeRealMusicOriginal/AudioLIME_500_samples_full_track/mix_without_components_predictions.json"
     client = Client("awsaf49/sonics-fake-song-detection")
 
-    result = client.predict(
-                audio_file=handle_file(orig_data),
-                model_type = "SpecTTTra-α",
-                duration="120s",
-                api_name="/predict"
-    )
-    print(f"Prediction result for {orig_data}: {result}")
+    results = {}
 
-    for file in os.listdir(dataset_path):
-        if file.endswith(".wav"):
-            print(f"Processing file: {file}")
-            result = client.predict(
-                audio_file=handle_file(os.path.join(dataset_path, file)),
-                model_type = "SpecTTTra-α",
-                duration="120s",
-                api_name="/predict")
-            print(f"Prediction result for {file}: {result}")
+    for model in os.listdir(dataset_path):
+        model_folder = os.path.join(dataset_path, model)
+        if not os.path.isdir(model_folder):
+            continue
+
+        print(f"Processing model: {model}")
+        audio_results = {}
+
+        for audio_file in os.listdir(model_folder):
+            reversed_separated_components_path = os.path.join(
+                model_folder, audio_file, "reversed_separated_components"
+            )
+            if not os.path.isdir(reversed_separated_components_path):
+                continue
+
+            file_results = {}
+            audio_stem = Path(audio_file).stem
+
+            for file in os.listdir(reversed_separated_components_path):
+                if file.endswith(".wav"):
+                    print(f"Processing file: {file}")
+                    result = client.predict(
+                        audio_file=handle_file(os.path.join(reversed_separated_components_path, file)),
+                        model_type="SpecTTTra-α",
+                        duration="120s",
+                        api_name="/predict",
+                    )
+
+                    file_results[file] = {
+                        "file_path": os.path.join(reversed_separated_components_path, file),
+                        "model": model,
+                        "prediction": result,
+                    }
+
+            audio_results[audio_stem] = {
+                "file_path": str(audio_file),
+                "model": model,
+                "results": file_results,
+            }
+
+        results[model] = audio_results
+
+    with open(output_json_path, "w") as json_file:
+        json.dump(results, json_file, indent=4)
+
+
+        
 
 if __name__ == "__main__":
     main()
-
-# orig: 0.929344892501831
-# bass0.wav: 0.4380253553390503
-# drums0.wav: 0.08595184236764908
-# other0.wav:  0.12505245208740234
-# vocals0.wav: 0.27443310618400574
-            
-# Prediction result for Data/FakeRealMusicOriginal/REAL/SaintLevant_IGuess.mp3.mp3: {'label': 'Fake', 'confidences': [{'label': 'Fake', 'confidence': 0.929344892501831}, {'label': 'Real', 'confidence': 0.07065510749816895}]}
-# Processing file: bass0.wav
-# Prediction result for bass0.wav: {'label': 'Fake', 'confidences': [{'label': 'Fake', 'confidence': 0.5619746446609497}, {'label': 'Real', 'confidence': 0.4380253553390503}]}
-# Processing file: drums0.wav
-# Prediction result for drums0.wav: {'label': 'Real', 'confidences': [{'label': 'Real', 'confidence': 0.9140481352806091}, {'label': 'Fake', 'confidence': 0.08595184236764908}]}
-# Processing file: other0.wav
-# Prediction result for other0.wav: {'label': 'Fake', 'confidences': [{'label': 'Fake', 'confidence': 0.8749475479125977}, {'label': 'Real', 'confidence': 0.12505245208740234}]}
-# Processing file: vocals0.wav
-# Prediction result for vocals0.wav: {'label': 'Real', 'confidences': [{'label': 'Real', 'confidence': 0.7255668640136719}, {'label': 'Fake', 'confidence': 0.27443310618400574}]}
-
-pred: 0.78
-bass0: 0.91
-drums0: 0.40
-other0: 0.47
-vocals0: 0.89
